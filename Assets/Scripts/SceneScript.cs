31bf9cba2c6b443fec83b6fba16ae97ba1aa7715
VictorVilin <47083451+VictorVilin@users.noreply.github.com>
2019-03-09 15:20:33 +0100

    Update SceneScript.cs
    Spawn des bonus en milieux de map

--- Assets/Scripts/SceneScript.cs
+++ Assets/Scripts/SceneScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
@@ -6,8 +6,9 @@ using UnityEngine;
 public class SceneScript : MonoBehaviour
 {
     //http://wiki.unity3d.com/index.php/Xbox360Controller
-
-    private bool confMK = true;
+    
+    private Vector3 MaxGlobalBonusPos = new Vector3(5f, 5f);    
+    private Vector3 MinGlobalBonusPos = new Vector3(-5f, -5f); // TODO changer pour utiliser la pos de la camera
 
     [SerializeField] private Text HealthTextB;
     [SerializeField] private Text HealthTextR;
@@ -30,37 +31,26 @@ public class SceneScript : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        SetCharacters(2);
+        SetCharacters(NbPlayer);
         Setconfig();
         SetHealthText();
     }
 
     private void Setconfig()
     {
-        if (confMK)
-        {
-            characters[0].configH = "Player1xaxis";
-            characters[0].configV = "Player1yaxis";
-            characters[0].configJump = "Player1Jump";
-            characters[0].configFire = "Player1Fire";
+        //Char Controls
+        characters[0].configH = "Player1xaxis";
+        characters[0].configV = "Player1yaxis";
+        characters[0].configJump = "Player1Jump";
+        characters[0].configFire = "Player1Fire";
 
-            characters[1].configH = "Player2xaxis";
-            characters[1].configV = "Player2yaxis";
-            characters[1].configJump = "Player2Jump";
-            characters[1].configFire = "Player2Fire";
-        }
-        else
-        {
-            characters[0].configH = "Player1xaxis";
-            characters[0].configV = "Player1yaxis";
-            characters[0].configJump = "Player1Jump";
-            characters[0].configFire = "Player1Fire";
+        characters[1].configH = "Player2xaxis";
+        characters[1].configV = "Player2yaxis";
+        characters[1].configJump = "Player2Jump";
+        characters[1].configFire = "Player2Fire";
 
-            characters[1].configH = "Player2xaxis";
-            characters[1].configV = "Player2yaxis";
-            characters[1].configJump = "Player2Jump";
-            characters[1].configFire = "Player2Fire";   
-        }
+        //Bonus
+        Camera cam = GameObject.Find("Main Camera").GetComponent<Camera>();
     }
     private void SetCharacters(int nbChar)
     {
@@ -73,23 +63,17 @@ public class SceneScript : MonoBehaviour
                 characters[i].Player.name = "Buddy " + (i + 1).ToString();
             }
         }
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("m"))
-        {
-            Setconfig();
-            confMK = !confMK;
-        }
         if (Input.GetKeyDown("r"))
         {
             restart = true;
             foreach (var item in characters) //position ini
             {
-                item.Player.transform.position = IniPos + new Vector3(-5 + 2.5f * characters.IndexOf(item), 0, 0);
+                item.Player.transform.position = IniPos + new Vector3(-5 + 5f * characters.IndexOf(item), 0, 0);
             }
             foreach (var item in LBonus) // On enlève les bonus apparus
             {
@@ -111,8 +95,7 @@ public class SceneScript : MonoBehaviour
         }
 
         SetHealthText();
-
-
+        
         RandomBonusSpawn();
 
         CheckLives();
@@ -136,9 +119,6 @@ public class SceneScript : MonoBehaviour
                     }
                 }
                 Time.timeScale = 0.2f;
-                {
-
-                }
                 return;
             }
         }
@@ -155,7 +135,7 @@ public class SceneScript : MonoBehaviour
     {
         if (Random.Range(0, 450f) < 1 /*&& LBonus.Count < 5*/)
         {
-            LBonus.Add(Instantiate(Bonus, new Vector3(Random.Range(-10, 10), Random.Range(-5, 5), 0), Quaternion.identity));
+            LBonus.Add(Instantiate(Bonus, new Vector3(Random.Range(MinGlobalBonusPos.x, MaxGlobalBonusPos.x), Random.Range(MaxGlobalBonusPos.y, MaxGlobalBonusPos.y), 0), Quaternion.identity));
         }
     }
     private void SetHealthText()
